9e0bb50c40a0c3e17df772c6d349c86e375c1efe
Isuru <isuru.c.p@gmail.com>
2012-07-17 15:11:04 +1200

    Added getVal/setVal functions.

--- cpu.js
+++ cpu.js
@@ -14,6 +14,7 @@ function GeneralRegister() {
 
 }
 
+// CP0 Register 12, Select 0
 function StatusRegister() {
 	this.CU = 1; // Controls access to co-processors, bits 31:28
 	this.RP = 0; // Enables reduced power mode, bit 27
@@ -33,11 +34,36 @@ function StatusRegister() {
 	this.ERL = 1; // Error Level, bit 2
 	this.EXL = 0; // Exception Level, bit 1
 	this.IE = 0; // Interrupt Enable, bit 0
+	
+	this.asUInt32 = function()
+	{
+		return ((this.CU * Math.pow(2, 28)) + (this.RP * Math.pow(2,27)) + (this.R * Math.pow(2,26)) + (this.RE * Math.pow(2, 25)) + (this.BEV * Math.pow(2,22)) + (this.TS * Math.pow(2, 21)) + (this.SR * Math.pow(2,20)) + (this.NMI * Math.pow(2, 19)) + (this.IM * Math.pow(2, 8)) + (this.UM * Math.pow(2,4)) + (this.ERL * Math.pow(2,2)) + (this.EXL * Math.pow(2,1)) + this.IE);   
+	}
+	
+	this.setValue = function(value)
+	{
+		this.CU = (value >>> 28) & 0xf;
+		this.RP = (value >>> 27) & 0x1;
+		this.R = (value >>> 26) & 0x1;
+		this.RE = (value >>> 25) & 0x1;
+		// this.zero ro
+		this.BEV = (value >>> 22) & 0x1;
+		this.TS = (value >>> 21) & 0x1;
+		this.SR = (value >>> 20) & 0x1;
+		this.NMI = (value >>> 19) & 0x1;
+		this.IM = (value >>> 8) & 0xff;
+		this.UM = (value >>> 4) & 0x1;
+		this.ERL = (value >>> 2) & 0x1;
+		this.EXL = (value >>> 1) & 0x1;
+		this.IE = value & 0x1;
+	}
+
 }
 
+// CP0 Register 16, Select 0
 function ConfigRegister()
 {
-	// all fields except K0 are read only
+	// all fields except K0 are read only by software
 	this.M = 1; // bit 31
 	this.K23 = 0; // bits 30:28
 	this.KU = 0; // bits 27:25
@@ -55,6 +81,67 @@ function ConfigRegister()
 	this.MT = 1; // bits 9:7
 	// bit 6 same as this.zero
 	this.K0 = 2; // bits 2:0
+	
+	this.asUInt32 = function()
+	{
+		return ((this.M * Math.pow(2,31)) + (this.K23 * Math.pow(2,28)) + (this.KU * Math.pow(2,25)) + (this.ISP * Math.pow(2,24)) + (this.DSP * Math.pow(2,23)) + (this.SB * Math.pow(2,21)) + (this.MDU * Math.pow(2,20)) + (this.MM * Math.pow(2,17)) + (this.BM * Math.pow(2,16)) + (this.BE * Math.pow(2,15)) + (this.AT * Math.pow(2,13)) + (this.AR * Math.pow(2,10)) + (this.MT * Math.pow(2,7)) + this.K0);
+	}
+	
+	this.setValue = function(value)
+	{
+		this.M = (value >>> 31) & 0x1;
+		this.K23 = (value >>> 28) & 0x7;
+		this.KU = (value >>> 25) & 0x7;
+		this.ISP = (value >>> 24) & 0x1;
+		this.DSP = (value >>> 23) & 0x1;
+		this.SB = (value >>> 21) & 0x1;
+		this.MDU = (value >>> 20) & 0x1;
+		this.MM = (value >>> 17) & 0x3;
+		this.BM = (value >>> 16) & 0x1;
+		this.BE = (value >>> 15) & 0x1;
+		this.AT = (value >>> 13) & 0x3;
+		this.AR = (value >>> 10) & 0x7;
+		this.MT = (value >>> 7) & 0x7;
+		this.K0 = value & 0x7;
+	}
+	
+}
+
+// CP0 Register 16, Select 1
+function Config1Register()
+{
+	// all fields read only by software
+	this.M = 0; // bit 31
+	this.MMUSize = 15; // bits 30:25
+	this.IS = 0; // bits 24:22
+	this.IL = 0; // bits 21:19
+	this.IA = 0; // bits 18:16
+	this.DS = 0; // bits 15:13
+	this.DL = 0; // bits 12:10
+	this.DA = 0; // bits 9:7
+	this.zero = 0; // bits 6:5
+	this.PC = 0; // bit 4, performance counter registers not program counter
+	this.WR = 1; // bit 3
+	this.CA = 0; // bit 2
+	this.EP = 1; // bit 1
+	this.FP = 0; // bit 0
+}
+
+// CP0 Register 15, Select 0
+function processorIDRegister()
+{
+	// all fields are read only by software
+	this.R = 0; // bits 31:24
+	this.companyID = 1; // bits 23:16
+	this.processorID = 128; // bits 15:8, (128 for 4kc)
+	this.revision = 11; // bits 7:0, latest version according to manual
+}
+
+// CP0 Register 17, Select 0
+function LLAddrRegister()
+{
+	this.zero = 0; // bits 31:28
+	this.PAddr = 0; // bits 27:0 - physical address read by most recent load linked instruction
 }
 
 
@@ -65,6 +152,9 @@ function MipsCpu () {
     this.genRegisters = new Array(32); // array of 32 32 bit integers
 	this.statusRegister = new StatusRegister();
 	this.configRegister = new ConfigRegister();
+	this.config1Register = new Config1Register();
+	this.processorIDRegister = new processorIDRegister();
+	this.llAddrRegister = new LLAddrRegister();
 	
 	this.getEndianness = function()
 	{
