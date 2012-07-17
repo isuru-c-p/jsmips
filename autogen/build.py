1b188d2da4081642212a3c470622d5ec2d71196e
Isuru <isuru.c.p@gmail.com>
2012-07-17 19:58:35 +1200

    work work

--- autogen/build.py
+++ autogen/build.py
@@ -1,20 +1,44 @@
 
 
+def getBitmask(range0, range1):
+	ret = 0
+	for r in [range0,range1]:
+		end, start = r
+		#print "end: %s, start: %s" % (end,start)
+		for i in range(int(start.rstrip('\r')), int(end.rstrip('\r'))+1):
+			ret |= (1 << i)
+	
+	return ret	
+	
 class Table(object):
-    def __init__(self,s):
-        self.cols = []
-        x = s.split('\n')
-        x = x[3:]
-        cols += [[op for ops in x.split(' ')]]
-        
-    def __repr__(self):
-        
+	def __init__(self,s):
+		rows = []
+		self.ops = []
+		fields = s.split('\n')
+		self.name = fields[0].rstrip('\r')
+		self.bitmask = getBitmask(fields[1].split(","), fields[2].split(","))
+		rows = fields[3:]
+		for row in rows:
+			for op in row.split(' '):
+				self.ops.append(op.rstrip('\r'))
+				
+	   
+	def __repr__(self):
+		return  self.name + '\n' + str(bin(self.bitmask)[2:]) + '\n' + str(self.ops)
 
 
 rawTables = open("optables.txt").read().split('----')
-rawTables = [t.trim() for t in rawTables]
+rawTables = [t.strip() for t in rawTables]
 tables = [ Table(t) for t in rawTables ]
 
+tablesDict = {}
+
+for table in tables:
+	tablesDict[table.name] = table
+
+startingTable = tablesDict["First"]
 allOpcodes = set([])
 
 for t in tables:
+	print t
+	
