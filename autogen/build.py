a5da4c73b95ce1a3cdd41bad09b5a9146f74544a
Isuru <isuru.c.p@gmail.com>
2012-07-17 20:57:37 +1200

    Completed code gen build.py

--- autogen/build.py
+++ autogen/build.py
@@ -1,30 +1,77 @@
 
 
-def getBitmask(range0, range1):
-	ret = 0
-	for r in [range0,range1]:
-		end, start = r
-		#print "end: %s, start: %s" % (end,start)
-		for i in range(int(start.rstrip('\r')), int(end.rstrip('\r'))+1):
-			ret |= (1 << i)
-	
-	return ret	
+
 	
 class Table(object):
 	def __init__(self,s):
+		self.ranges = []
 		rows = []
 		self.ops = []
 		fields = s.split('\n')
 		self.name = fields[0].rstrip('\r')
-		self.bitmask = getBitmask(fields[1].split(","), fields[2].split(","))
+		self.setBitmask(fields[1].split(","), fields[2].split(","))
+		
 		rows = fields[3:]
 		for row in rows:
 			for op in row.split(' '):
 				self.ops.append(op.rstrip('\r'))
-				
-	   
+	
+	def setBitmask(self, range0, range1):
+		ret = 0
+		
+		for r in [range0,range1]:
+			end, start = r
+			self.ranges.append([int(start.rstrip('\r')), int(end.rstrip('\r'))+1])
+			#print "end: %s, start: %s" % (end,start)
+			for i in range(int(start.rstrip('\r')), int(end.rstrip('\r'))+1):
+				ret |= (1 << i)
+		
+		self.bitmask = ret	
+	
 	def __repr__(self):
-		return  self.name + '\n' + str(bin(self.bitmask)[2:]) + '\n' + str(self.ops)
+		bitmask = self.bitmask
+		cases = ""
+		
+		for i,op in enumerate(self.ops):
+			noCols = (2 ** (self.ranges[0][1] - self.ranges[0][0]))
+			left = i % noCols
+			top = (i - left)/noCols
+			
+			opcodeCaseValue = (left << self.ranges[0][0]) + (top << self.ranges[1][0])
+			
+			if (op in tablesDict.keys()):
+				subswitch = str(tablesDict[op])
+				cases += \
+				"""
+					case %(opcodeCaseValue)s:
+						%(subswitch)s;
+						break;
+						
+				""" % locals()			
+			
+			else:
+				cases += \
+				"""
+					case %(opcodeCaseValue)s:
+						this.%(op)s();
+						break;
+						
+				""" % locals()
+			
+		
+		switchCode = \
+"""
+bitmask = %(bitmask)s;
+switch(op & bitmask)
+{
+	%(cases)s
+}
+
+}
+""" % locals()
+		
+		return switchCode
+		#return  self.name + '\n' + str(bin(self.bitmask)[2:]) + '\n' + str(self.ops)
 
 
 rawTables = open("optables.txt").read().split('----')
@@ -39,6 +86,20 @@ for table in tables:
 startingTable = tablesDict["First"]
 allOpcodes = set([])
 
-for t in tables:
-	print t
+#for t in tables:
+#	print t
+
+startingTableStr = str(startingTable)
+
+tableSwitch = \
+"""
+	doOp = function(op) {
+		%(startingTableStr)s
+	}
+""" % locals()
+
+outFile = open("doOp.autogen", "w")
+outFile.write(tableSwitch)
+
+#print startingTable
 	
