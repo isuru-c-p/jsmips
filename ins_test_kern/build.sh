58255334a200b5745f6fba9d527ea61085f46869
andrew <andrew>
2012-07-19 17:21:40 +1200

    kernel map

--- ins_test_kern/build.sh
+++ ins_test_kern/build.sh
@@ -4,7 +4,7 @@ mips-linux-gcc -o kernel.o -c kernel.c -Wall -Wextra -Werror \
     -nostdlib -fno-builtin -nostartfiles -nodefaultlibs
 mips-linux-as -o entry.o entry.s
 mips-linux-ld -T linker.ld -o kernel.bin entry.o kernel.o
+mips-linux-objdump -D kernel.bin > kernel.map
 python tohex.py kernel.bin > kernel.hex
-
 echo "built kernel with entry point:" 
 echo `mips-linux-nm kernel.bin | grep entry`
