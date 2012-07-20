9269fe3fba93fe6924f61ec0c8eaad45d8334dd4
andrew chambers <andrew@chambers.net.nz>
2012-07-20 22:37:04 +1200

    fixed logger and added command line

--- node_main.js
+++ node_main.js
@@ -1,23 +1,64 @@
 fs = require('fs')
 
 
+function extractArgument( arg, defaultVal  ) {
+    
+    for(var i = 2; i < process.argv.length ; i ++ ){
+        var argName = process.argv[i].substr(0,arg.length);
+        if(argName == arg){
+            return process.argv[i].substr(arg.length+1);
+        }
+    }
+    
+    return defaultVal;
+}
+
+
+var image = extractArgument("--image","")
+
+if(image == ""){
+    ERROR("cannot run without an image!");
+    process.exit(1);
+}
+
+INFO("loading image : " + image);
+
+var entryPoint =extractArgument("--entrypoint","");
+
+if(entryPoint == ""){
+    ERROR("cannot run without an entry point!");
+    process.exit(1);
+}
+
+entryPoint =  parseInt(entryPoint,16);
+
+
+INFO("loading image with entry point: " + entryPoint.toString(16));
+
+var tracefile = extractArgument("--tracefile","")
+var shouldTrace = false;
+if(tracefile == "") {
+    INFO("Not tracing emulator");
+} else {
+    shouldTrace = true;
+    INFO("saving a trace to file: " + tracefile)
+}
+
 
 emu = new Emulator()
+emu.mmu.physicalMemory.loadHexString(new String(fs.readFileSync(image)),0)
 
-DEBUG("loading "+ process.argv[2]);
-emu.mmu.physicalMemory.loadHexString(new String(fs.readFileSync(process.argv[2])),0)
-entryPoint = parseInt(process.argv[3], 16);
-
-
-tracer = new Tracer("mips.trace")
-
+if (shouldTrace){
+    tracer = new Tracer(tracefile);
+}
 emu.cpu.PC.putUInt32(entryPoint)
-
 
 setInterval(function () {
 
     for(var i = 0 ; i < 500 ; i++){
-        //tracer.writeTrace(emu);
+        if(shouldTrace){
+            tracer.writeTrace(emu);
+        }
         emu.step();
     }
 
