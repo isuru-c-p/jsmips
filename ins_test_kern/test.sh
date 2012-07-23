8a2ee97a2512d1200bc70aeef94744f2fd03818d
Isuru <isuru.c.p@gmail.com>
2012-07-23 14:02:59 +1200

    Updated path in test.sh

--- ins_test_kern/test.sh
+++ ins_test_kern/test.sh
@@ -3,10 +3,10 @@ set -e
 for KERNEL in `ls *.hex`
 do
     BASE=`basename $KERNEL .hex`
-    if ! node ../node_4kc.js --image=$KERNEL --entrypoint=`cat $BASE.entry` ; then
+    if ! node ../js/node_4kc.js --image=$KERNEL --entrypoint=`cat $BASE.entry` ; then
         echo "test $BASE failed! running a trace..."
         set +e
-        node ../node_4kc.js --image=$KERNEL --entrypoint=`cat $BASE.entry` --tracefile=$BASE.trace
+        node ../js/node_4kc.js --image=$KERNEL --entrypoint=`cat $BASE.entry` --tracefile=$BASE.trace
         set -e
     fi
     
