b578bbb7630b6ca786a62138e01de989309c0f30
Isuru <isuru.c.p@gmail.com>
2012-08-02 15:02:50 +1200

    Increased cycles per interval to 10000 for browser version.

--- js/browser/browser_main.js
+++ js/browser/browser_main.js
@@ -8,15 +8,9 @@ PCLogCounter = 0;
 
 setInterval(function () {
 
-    PCLogCounter += 1;
-    for(var i = 0 ; i < 500 ; i++){
+    for(var i = 0 ; i < 10000 ; i++){
         emu.step();
     }
     
-    /*if(PCLogCounter >= 10000){
-        PCLogCounter = 0;
-        INFO("PCsampler: " + emu.cpu.PC.asUInt32().toString(16))
-    }*/
-
 }, 0);
 });
